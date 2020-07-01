d76bce89fcc6101ef37c1e7b417e3c0dda30c5cb
nicolasgabrieloficial <61585068+nicolasgabrieloficial@users.noreply.github.com>
2020-07-01 06:28:23 -0300

    Add files via upload

---
+++ Aula23Listas/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula23Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Criando a lista
+           List<Produto> produtos = new List<Produto>();
+
+           //Adicionado itens a lista instaciando eles
+           produtos.Add(new Produto(1,"FIFA20", 129.99f));
+           produtos.Add(new Produto(2,"GTA V", 79.99f));
+           produtos.Add(new Produto(3,"Far Cry 5", 129.99f));
+           produtos.Add(new Produto(4,"The Last of Us II", 239.99f));
+           produtos.Add(new Produto(5,"Sea of Thieves", 89.99f));
+
+           // ou um por um usando instâncias básicas e seus atributos
+           Produto reddead = new Produto();
+           reddead.Codigo = 6;
+           reddead.Nome = "Red Dead Redemption 2";
+           reddead.Preco = 249.99f;
+
+           produtos.Add(reddead);
+
+           //mostrando itens da lista com o FOREACH
+           foreach(Produto p in produtos){
+               Console.WriteLine($"{p.Nome} - {p.Preco}");
+            }
+            //podemos remove um item pelo seu índice no se array/arranjo
+            produtos.RemoveAt(4);
+
+            //podemos tbm remover da lista usando lambda e o método removeAll:
+            produtos.RemoveAll(x => x.Nome == "Sea of Thieves");
+
+            Console.WriteLine("\nLista Alterada: ");
+            //checamos se foi alterada
+            foreach(Produto p in produtos){
+                System.Console.WriteLine($"{p.Nome} - {p.Preco} ");
+            }
+        }
+    }
+}
